368cbcdd397de8d69165fe042ae3768153358082
OneKr-Billy <378012098@qq.com>
2024-07-17 19:47:59 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.ai-one.org": dockerHub,
+  "quay.ai-one.org": "https://quay.io",
+  "gcr.ai-one.org": "https://gcr.io",
+  "k8s-gcr.ai-one.org": "https://k8s.gcr.io",
+  "k8s.ai-one.org": "https://registry.k8s.io",
+  "ghcr.ai-one.org": "https://ghcr.io",
+  "cloudsmith.ai-one.org": "https://docker.cloudsmith.io",
+  "ecr.ai-one.org": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.ai-one.org": dockerHub,
 };
 
 function routeByHosts(host) {
